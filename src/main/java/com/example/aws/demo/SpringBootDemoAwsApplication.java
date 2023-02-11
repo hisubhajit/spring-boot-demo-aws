fcd12ca708f701c0d81a110b5b0e0c94b05dcd93
Subhajit Dinda <subhajit.dinda@gmail.com>
2023-02-11 19:34:26 +0530

    base project with web dependency

---
+++ src/main/java/com/example/aws/demo/SpringBootDemoAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoAwsApplication.class, args);
+	}
+
+}
